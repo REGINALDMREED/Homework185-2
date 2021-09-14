116fa36a81f2e3b22532db5de92947ce51855338
Reed <reginaldmreed@student.midlandstech.edu>
2021-09-13 20:42:34 -0400

    commit all

--- Homework185-2/Form1.Designer.vb
+++ Homework185-2/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class MainForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -27,13 +27,13 @@ Partial Class Form1
         Me.picMarathon = New System.Windows.Forms.PictureBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.lblInfo = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         CType(Me.picMarathon, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'btnView
         '
-        Me.btnView.Location = New System.Drawing.Point(35, 64)
+        Me.btnView.Location = New System.Drawing.Point(53, 74)
         Me.btnView.Name = "btnView"
         Me.btnView.Size = New System.Drawing.Size(182, 23)
         Me.btnView.TabIndex = 0
@@ -42,7 +42,7 @@ Partial Class Form1
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(53, 309)
+        Me.btnExit.Location = New System.Drawing.Point(66, 252)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(182, 23)
         Me.btnExit.TabIndex = 1
@@ -51,9 +51,11 @@ Partial Class Form1
         '
         'picMarathon
         '
-        Me.picMarathon.Location = New System.Drawing.Point(250, 64)
+        Me.picMarathon.Image = Global.Homework185_2.My.Resources.Resources.Marathon
+        Me.picMarathon.InitialImage = Global.Homework185_2.My.Resources.Resources.Marathon
+        Me.picMarathon.Location = New System.Drawing.Point(329, 62)
         Me.picMarathon.Name = "picMarathon"
-        Me.picMarathon.Size = New System.Drawing.Size(100, 256)
+        Me.picMarathon.Size = New System.Drawing.Size(247, 256)
         Me.picMarathon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.picMarathon.TabIndex = 2
         Me.picMarathon.TabStop = False
@@ -64,42 +66,42 @@ Partial Class Form1
         Me.Label1.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(40, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(177, 25)
+        Me.Label1.Size = New System.Drawing.Size(157, 25)
         Me.Label1.TabIndex = 3
-        Me.Label1.Text = "Application title"
+        Me.Label1.Text = "Fall Marathon"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(50, 256)
+        Me.Label2.Location = New System.Drawing.Point(12, 153)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(43, 14)
+        Me.Label2.Size = New System.Drawing.Size(323, 56)
         Me.Label2.TabIndex = 4
-        Me.Label2.Text = "Phrase"
+        Me.Label2.Text = "May 30, 2017" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Ryan Park" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Start time 8:00 A.M." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "For more information,view Fall Mar" &
+    "athon Facebook page 3" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
-        'lblInfo
+        'Label3
         '
-        Me.lblInfo.AutoSize = True
-        Me.lblInfo.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblInfo.Location = New System.Drawing.Point(50, 232)
-        Me.lblInfo.Name = "lblInfo"
-        Me.lblInfo.Size = New System.Drawing.Size(91, 14)
-        Me.lblInfo.TabIndex = 5
-        Me.lblInfo.Text = "Marathon detail"
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 34)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(212, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Full and half marathon runners are welcome"
         '
-        'Form1
+        'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(362, 402)
-        Me.Controls.Add(Me.lblInfo)
+        Me.ClientSize = New System.Drawing.Size(607, 465)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.picMarathon)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnView)
-        Me.Name = "Form1"
+        Me.Name = "MainForm"
         CType(Me.picMarathon, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -111,5 +113,5 @@ Partial Class Form1
     Friend WithEvents picMarathon As PictureBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents lblInfo As Label
+    Friend WithEvents Label3 As Label
 End Class
--- Homework185-2/My Project/Application.Designer.vb
+++ Homework185-2/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Homework185_2.Form1
+            Me.MainForm = Global.Homework185_2.MainForm
         End Sub
     End Class
 End Namespace
